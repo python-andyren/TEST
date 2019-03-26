d995a8f294086c82e2bccddeacd2d4a7245ff0a2
andyren <51946816@qq.com>
2019-03-26 10:02:45 +0800

    add

---
+++ zhaoyaojing_huoli.py
@@ -0,0 +1,315 @@
+# coding=utf-8
+import requests
+import time
+import json
+from lxml import etree
+import pymysql
+from sshtunnel import SSHTunnelForwarder
+import csv
+
+# fp = open('0322买号信息.txt', 'a+', encoding='utf8')
+
+add_cookie = input('Cookie:')
+add_TimeStamp = input('TimeStamp:')
+
+def to_sql(li):
+    # with SSHTunnelForwarder(
+    #         ('120.132.65.133', 22),  # B机器的配置
+    #         ssh_password=r'*:A8QDe(98KIJx07l\bRm5k/#/3a',
+    #         ssh_username='root',
+    #         remote_bind_address=('10.9.72.185', 3306)) as server:  # A机器的配置
+
+    db_connect = pymysql.connect(host='127.0.0.1',  # 此处必须是是127.0.0.1
+                                 port=3306,
+                                 user='root',
+                                 passwd='8jN5N8T5v4K5',
+                                 db='data_third')
+
+    cursor = db_connect.cursor()
+
+    ww = li[0]
+
+    # bid = li[1]
+
+    base_info = li[1]
+
+    tag = li[2]['标签']
+
+    deep_info = li[3]
+
+    add_time = li[4]
+
+    # mjxy = base_info['买家信誉']
+    # sjxy = base_info['商家信誉']
+    # sex = base_info['性别']
+    # sdhpl = base_info['收到好评率']
+    # zcrq = base_info['注册日期']
+    # tl = base_info['淘龄']
+    # mjzzpj = base_info['买家总周平均']
+    # cxsj = base_info['查询时间']
+    # nameconform = base_info['是否实名']
+    # vip_level = base_info['会员类型']
+    # tbhy = base_info['淘宝会员']
+    # tz = base_info['打标情况-兔子(拿完了商家的返款就恶意退款跑了！)']
+    # mh = base_info['打标情况-蜜獾(用各种方式威胁你给钱！)']
+    # hl = base_info['打标情况-狐狸(用各种方式骗你钱了！)']
+    # eyu = base_info['打标情况-鳄鱼(用工商，发票，字体，商标，假货各种方式来坑你钱了！)']
+    # jq = base_info['打标情况-降权处置']
+    # black = base_info['云黑名单']
+
+    # Average_number = deep_info['近一季度平均周成交次数:']
+    # xfl = deep_info['消费力:']
+    # yhdy = deep_info['用户地域:']
+    # fchpl = deep_info['发出好评率:']
+    # gmpl = deep_info['购买频率:']
+    # yhtkl = deep_info['用户退款率:']
+    # zdph = deep_info['终端偏好:']
+
+    # 拼接sql语句
+    sql = 'insert into shuangying_huolifu(ww,buyreputation,bussreputation,sex,favrate,regdate,amoyage,buyweeklyaver,querytime,real_name,vip_level,vip_info,tucao,tuzi,mihuan,huli,eyu,disposalsitus,blacklist,label,averagetimes,consumpower,userarea,sendrate,buyfrequency,userrefund,terminal,add_time) values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%d")' % (
+        ww,
+        base_info['买家信誉'], base_info['商家信誉'], base_info['性别'], base_info['收到好评率'], base_info['注册日期'], base_info['淘龄'], base_info['买家总周平均'], base_info['查询时间'], base_info['是否实名'], base_info['会员类型'], base_info['淘宝会员'],
+        base_info['吐槽'],base_info['打标情况-兔子'], base_info['打标情况-蜜獾'], base_info['打标情况-狐狸'], base_info['打标情况-鳄鱼'], base_info['打标情况-降权处置'], base_info['云黑名单'],
+        tag,deep_info['近一季度平均周成交次数'],deep_info['消费力'],deep_info['用户地域'],deep_info['发出好评率'],deep_info['购买频率'],deep_info['用户退款率'],deep_info['终端偏好'],add_time)
+    # 执行sql语句
+    try:
+        cursor.execute(sql)
+        db_connect.commit()
+        print('插入成功')
+    except Exception as e:
+        print(e)
+        db_connect.rollback()
+    cursor.close()
+    db_connect.close()
+
+def get_tag(name):
+    headers_tag = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Length': '51',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': add_cookie,
+        'Host': 'app.tk1788.com',
+        'Origin': 'http://app.tk1788.com',
+        'Referer': 'http://app.tk1788.com/app/superscan/searchAliim.jsp',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+    tag_data = {
+        'm': 'tagSearch',
+        'aliim': name
+    }
+
+    content3 = requests.post('http://app.tk1788.com/app/superscan/op.jsp', data=tag_data, headers=headers_tag).text
+
+    time.sleep(4)
+
+    return content3
+
+def get_sign(name):
+    headers_getSign = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Length': '76',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': add_cookie,
+        'Host': 'app.tk1788.com',
+        'Origin': 'http://app.tk1788.com',
+        'Referer': 'http://app.tk1788.com/app/superscan/searchAliim.jsp',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+    data_getSign = {
+        'm': 'getSign',
+        'aliim': name,
+        'timeStamp': add_TimeStamp,
+    }
+
+    content1 = requests.post(url='http://app.tk1788.com/app/superscan/op.jsp', headers=headers_getSign,
+                             data=data_getSign).text
+
+    sign_data = json.loads(content1)
+
+    return sign_data
+
+def Deep_Search(regist_day, sign, name):
+    headers_DeepSearch = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Length': '142',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': add_cookie,
+        'Host': 'app.tk1788.com',
+        'Origin': 'http://app.tk1788.com',
+        'Referer': 'http://app.tk1788.com/app/superscan/searchAliim.jsp',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+
+    data_DeepSearch = {
+        'm': 'deepSearchAliim',
+        'aliim': name,
+        'buyNum': '',
+        'registDay': regist_day,
+        'timeStamp': add_TimeStamp,
+        'sign': sign,
+    }
+
+    content4 = requests.post(url='http://app.tk1788.com/app/superscan/op.jsp', headers=headers_DeepSearch,
+                             data=data_DeepSearch).text
+
+    time.sleep(2)
+
+    data_Deep_Search = json.loads(content4)
+
+    return data_Deep_Search
+
+add_time = int(time.strftime('%Y%m%d', time.localtime(time.time())))
+
+huoli_url = 'http://plm.huolifu.com/comm_plat_huoli/out_bind_info'
+re = requests.get(url=huoli_url).json()
+for i in re:
+    name = i['account_name']
+
+    sign = get_sign(name)['sign']
+
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Length': '193',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': add_cookie,
+        'Host': 'app.tk1788.com',
+        'Origin': 'http://app.tk1788.com',
+        'Referer': 'http://app.tk1788.com/app/superscan/searchAliim.jsp',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+
+    form_data = {
+        'm': 'sAliim',
+        'aliim': name,
+        'costType': 'Type4Click',
+        'timeStamp': add_TimeStamp,
+        'sign': sign,
+        'ifQBase': 'true',
+        'ifQReport': 'true',
+        'ifQDownB': 'true',
+        'judgeAnother': 'true',
+    }
+
+    try:
+        content = requests.post(url='http://app.tk1788.com/app/superscan/op.jsp', headers=headers, data=form_data).text
+
+        time.sleep(2)
+
+        content2 = json.loads(content)
+
+        GetTag = json.loads(get_tag(name))['msg']
+
+        tree = etree.HTML(GetTag)
+
+        tag = tree.xpath('//span/text()')[0]
+
+        regist_day = content2['registDay']
+
+        DeepData = Deep_Search(regist_day, sign, name)
+
+        msg = DeepData['msg']
+
+        tree = etree.HTML(msg)
+
+        questions = tree.xpath('//td/text()')
+
+        result = tree.xpath('//span/text()')
+
+        if len(result) == 8:
+            a = result[0]
+            b = result[1]
+            c = result[2]
+            d = result[3]
+            e = result[4]
+            f = result[5]
+            g = result[6]
+            h = result[7]
+        elif len(result) == 7:
+            a = result[0]
+            b = result[1]
+            c = result[2]
+            d = result[3]
+            e = '暂无数据'
+            f = result[4]
+            g = result[5]
+            h = result[6]
+        else:
+            a = '暂无数据'
+            b = '暂无数据'
+            c = '暂无数据'
+            d = '暂无数据'
+            e = '暂无数据'
+            f = '暂无数据'
+            g = '暂无数据'
+            h = '暂无数据'
+
+        three_data = {
+            '近一季度平均周成交次数': a,
+            '消费力': b,
+            '用户地域': c,
+            '发出好评率': d,
+            '购买频率': e,
+            '用户退款率': f,
+            '收到好评率:': g,
+            '终端偏好': h,
+        }
+
+        one_data = {
+            '买家信誉': content2['buyerCre'],
+            '商家信誉': content2['sellerCredit'],
+            '性别': content2['sex'],
+            '收到好评率': content2['received_rate'],
+            '注册日期': content2['created'],
+            '淘龄': content2['registDay'],
+            '买家总周平均': content2['buyerAvg'],
+            '查询时间': content2['queryTime'],
+            '是否实名': content2['nameconform'],
+            '会员类型': content2['vip_level'],
+            '淘宝会员': content2['vip_info'],
+            '吐槽': content2['aliimComplaintsNum'],
+            '打标情况-兔子': content2['type1'],
+            '打标情况-蜜獾': content2['type2'],
+            '打标情况-狐狸': content2['type3'],
+            '打标情况-鳄鱼': content2['type4'],
+            '打标情况-降权处置': content2['downNum'],
+            '云黑名单': content2['yunBlack']
+        }
+
+        biaoqian = {
+            '标签': tag
+        }
+
+        li = []
+        li.append(name)
+        li.append(one_data)
+        li.append(biaoqian)
+        li.append(three_data)
+        li.append(add_time)
+        print(li)
+
+        to_sql(li)
+
+        time.sleep(5)
+    except:
+        print('账号存在异常')
+        time.sleep(3)
+
+    time.sleep(2)
+
+# fp.close()
