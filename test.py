d79b6de141a139ae46e723d3bd5d3ccd77c598e2
andyren <51946816@qq.com>
2019-03-27 08:54:45 +0800

    add

--- test.py
+++ test.py
@@ -7,28 +7,12 @@ import re
 import pymysql
 from sshtunnel import SSHTunnelForwarder
 
+huoli_url = 'http://plm.huolifu.com/comm_plat_huoli/out_bind_info'
+content1 = requests.get(url=huoli_url).json()
 
+print(content1)
 
-good_url = 'http://www.wuliy.com/#/details?activityId={}'
+huoying_url = 'http://plm.huoyingfu.com/comm_plat_shuangying/out_bind_info'
+content2 = requests.get(url=huoying_url).json()
 
-
-def get_img(good_id):
-    url = good_url.format(good_id)
-
-    get_img_headers = {
-        'Referer': 'http://www.wuliy.com/',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
-
-    }
-
-    content3 = requests.get(url=url, headers=get_img_headers).content.decode('utf8')
-
-    print(content3)
-    #
-    # tree1 = etree.HTML(content3)
-    #
-    # img_list = tree1.xpath('//div[@class="tabCon"]/ul/li/div[2]/p/img/@src')
-    #
-    # return img_list
-
-get_img(1107124878173011968)
+print(content2)
